12bb48276ac47b67f6e6ed5ad87eb70eb708f298
Gilson Alves <gilsonalves6@icloud.com>
2019-01-07 12:42:16 -0200

    crud do admin

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 	session_start();
 	require_once("vendor/autoload.php");
 
-	use Hcode\Page;
-	use Slim\Slim;
-	use Hcode\PageAdmin;
 	use Hcode\Model\User;
-
+	use Hcode\Page;
+	use Hcode\PageAdmin;
+	use Slim\Slim;
 
 	$app = new Slim();
 
@@ -15,14 +14,17 @@
 	$app->get('/', function () {
 
 		$page = new Page();
+
 		$page->setTpl("index");
 
 	});
 
 	$app->get('/admin/', function () {
+
 		User::verifyLogin();
 
 		$page = new PageAdmin();
+
 		$page->setTpl("index");
 
 	});
@@ -33,24 +35,164 @@
 				'header' => false,
 				'footer' => false
 		]);
+
 		$page->setTpl("login");
 
 	});
 
-	$app->post('/admin/login', function (){
+	$app->post('/admin/login', function () {
+
 		User::login($_POST['login'], $_POST["password"]);
+
 		header("Location: /admin");
 		exit;
+
 	});
 
-	$app->get('/admin/logout', function(){
+	$app->get('/admin/logout', function () {
+
 		User::logout();
+
 		header("Location: /admin/login");
 		exit;
 
+	});
+
+	$app->get("/admin/users", function () {
+
+		User::verifyLogin();
+
+		$users = User::listAll();
+
+		$page = new PageAdmin();
+
+		$page->setTpl("users", array(
+				"users" => $users
+		));
+
+	});
+
+	$app->get("/admin/users/create", function () {
+
+		User::verifyLogin();
+
+		$page = new PageAdmin();
+
+		$page->setTpl("users-create");
+
+	});
+
+	$app->get("/admin/users/:iduser/delete", function ($iduser) {
+
+		User::verifyLogin();
+
+		$user = new User();
+
+		$user->get((int) $iduser);
+
+		$user->delete();
+
+		header("Location: /admin/users");
+		exit;
+
+	});
+
+	//Método para pegar as informações do Usuario
+	$app->get("/admin/users/:iduser", function ($iduser) {
+
+		User::verifyLogin();
+
+		$user = new User();
+
+		$user->get((int) $iduser);
+
+		$page = new PageAdmin();
+
+		$page->setTpl("users-update", array(
+				"user" => $user->getvalues()
+		));
+
+	});
+
+	$app->post("/admin/users/create", function () {
+		User::verifyLogin();
+
+		$user = new User();
+
+		$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+
+		$user->setData($_POST);
+
+		$user->save();
+
+		header("Location: /admin/users");
+		exit;
+
+	});
+
+	//Método que atualiza as informações do Usuario na base de dados
+	$app->post("/admin/users/:iduser", function ($iduser) {
+		User::verifyLogin();
+
+		$user = new User();
+
+		$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+
+		$user->get((int) $iduser);
+
+		$user->setData($_POST);
+
+		$user->update();
+
+		header("Location: /admin/users");
+		exit;
+
+	});
+
+	/**
+	 * Rota do formulario esqueci a senha
+	 */
+	$app->get("/admin/forgot", function () {
+		$page = new PageAdmin([
+				"header" => false,
+				"footer" => false
+		]);
+		$page->setTpl("forgot");
+	});
+
+	/**
+	 * rota para o envio do formulário esqueci a senha
+	 */
+	$app->post("/admin/forgot", function (){
+
+		$user = User::getForgot($_POST["email"]);
 	});
 
 
 	$app->run();
 
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -42,14 +42,77 @@
 		}
 
 		public static function verifyLogin($inadmin = true) {
-			if (!isset($_SESSION[User::SESSION]) || !$_SESSION[User::SESSION] || !(int) $_SESSION[User::SESSION]["iduser"] > 0 || (bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin) {
+			if (!isset($_SESSION[User::SESSION]) || !$_SESSION[User::SESSION] || !(int) $_SESSION[User::SESSION]["iduser"] > 0 || (bool) $_SESSION[User::SESSION]["inadmin"] !== $inadmin) {
 				header("Loacation: /admin/login");
 				exit;
 			}
 		}
 
-		public static function logout(){
+		public static function logout() {
 			$_SESSION[User::SESSION] = null;
 		}
+
+		public static function listAll() {
+			$sql = new Sql();
+			return $sql->select("select * from tb_users a inner join tb_persons b using(idperson) order by b.desperson");
+		}
+
+		public function save() {
+			$sql = new Sql();
+			$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+					":desperson" => utf8_decode($this->getdesperson()),
+					":deslogin" => $this->getdeslogin(),
+					":despassword" => $this->getdespassword(),
+					":desemail" => $this->getdesemail(),
+					":nrphone" => $this->getnrphone(),
+					":inadmin" => $this->getinadmin()
+			));
+			$this->setData($results[0]);
+		}
+
+		public function get($iduser) {
+			$sql = new Sql();
+			$results = $sql->select("select * from tb_users a inner join tb_persons b using (idperson) where a.iduser = :iduser", array(
+					":iduser" => $iduser
+			));
+			$this->setData($results[0]);
+		}
+
+		public function update() {
+			$sql = new Sql();
+			$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+					":iduser"=> $this->getiduser(),
+					":desperson" => utf8_decode($this->getdesperson()),
+					":deslogin" => $this->getdeslogin(),
+					":despassword" => $this->getdespassword(),
+					":desemail" => $this->getdesemail(),
+					":nrphone" => $this->getnrphone(),
+					":inadmin" => $this->getinadmin()
+			));
+			$this->setData($results[0]);
+		}
+
+		public function delete(){
+			$sql = new Sql();
+			$sql->query("call sp_users_delete(:iduser)", array(
+				":iduser" => $this->getiduser()
+			));
+		}
+
+		public static function getForgot($email){
+			$sql = new Sql();
+			$reults = $sql->select("select * from tb_persons a inner join tb_users b using (idperson) where a.desemail = :email", array(
+					":email" => $email
+			));
+
+			if (count($reults) === 0 ){
+				throw new \Exception("Não foi possível recuperar a senha!");
+			}else{
+				$sql->select("call sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+
+				));
+			}
+		}
+
 
 	}
